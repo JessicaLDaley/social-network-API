668bcc0f1f0f28ca84eef31bf3e5e3b0d346a7a6
JessicaLDaley <jessicadaleypaints@yahoo.com>
2021-12-26 19:27:05 -0500

    added POST capability for reactions

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -84,6 +84,23 @@ const thoughtController = {
         })
         .catch(err => res.status(500).json(err));
     },
+     // POST /api/thoughts/:id/reactions
+     addReaction({ params, body }, res) {
+        Thought.findOneAndUpdate(
+            { _id: params.thoughtId },
+            { $addToSet: { reactions: body } },
+            { new: true, runValidators: true }
+        )
+        .then(dbThoughtData => {
+            if (!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with that id' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(500).json(err));
+    },
+
     
     }
 
